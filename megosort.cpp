d20932871160a4d3766fab7bc10d41d0cd4b2e41
mansoor <t.alyamani95@gmail.com>
2025-04-15 11:26:59 +0700

    Ensured all remaining elements are added to the sorted array

--- megosort.cpp
+++ megosort.cpp
@@ -114,3 +114,51 @@ void mergeSort(int low, int high)
         arr[x] = B[x];
     }
 }
+void mergeSort(int low, int high)
+{
+    if (low >= high)
+    {
+        return;
+    }
+
+    int mid = (low + high) / 2;
+
+    mergeSort(low, mid);
+    mergeSort(mid + 1, high);
+
+    int i = low, j = mid + 1, k = low;
+
+    while (i <= mid && j <= high)
+    {
+        if (arr[i] <= arr[j])
+        {
+            B[k] = arr[i];
+            i++;
+        }
+        else
+        {
+            B[k] = arr[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (j <= high)
+    {
+        B[k] = arr[j];
+        j++;
+        k++;
+    }
+
+    while (i <= mid)
+    {
+        B[k] = arr[i];
+        i++;
+        k++;
+    }
+
+    for (int x = low; x <= high; x++)
+    {
+        arr[x] = B[x];
+    }
+}
