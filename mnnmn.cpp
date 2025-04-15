9178b7e6c376732503ef029393196ec3d134b2a4
mansoor <t.alyamani95@gmail.com>
2025-04-15 11:25:26 +0700

    Added recursive sorting and merging logic

---
+++ mnnmn.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+// Commit 1:
+// Add array declarations and size variable
+// - Created main array 'arr' and temporary array 'B'
+// - Declared variable 'n' to store the number of elements
+int arr[20], B[20];
+int n;
