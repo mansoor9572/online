1e47fc33ea36206264febadde6dd577d4a23393d
mansoor <t.alyamani95@gmail.com>
2025-04-15 11:23:07 +0700

    Added check for array size limit

--- megosort.cpp
+++ megosort.cpp
@@ -1,5 +1,61 @@
 #include <iostream>
 using namespace std;
- 
+
+// 
 int arr[20], B[20];
 int n;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan Panjang element array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nMaksimal panjang array adalah 20";
+        }
+    }
+
+    cout << "\n------------------------" << endl;
+    cout << "\nInputkan Isi element array" << endl;
+    cout << "\n------------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Array index ke-" << i << " : ";
+        cin >> arr[i];
+    }
+}
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan Panjang element array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nMaksimal panjang array adalah 20";
+        }
+    }
+
+    cout << "\n------------------------" << endl;
+    cout << "\nInputkan Isi element array" << endl;
+    cout << "\n------------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Array index ke-" << i << " : ";
+        cin >> arr[i];
+    }
+}
